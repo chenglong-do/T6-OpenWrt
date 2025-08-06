ad28221d6cd69e80c27b2f33277cfd9a87f3a7bb
Chenglong Du <dchenglong@cnksi.com>
2025-08-06 10:12:26 +0800

    Update diy-part2.sh

--- istoreos/diy-part2.sh
+++ istoreos/diy-part2.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 # update ubus git HEAD
-cp -f $GITHUB_WORKSPACE/configfiles/ubus_Makefile package/system/ubus/Makefile
+#cp -f $GITHUB_WORKSPACE/configfiles/ubus_Makefile package/system/ubus/Makefile
 
 # 近期istoreos网站文件服务器不稳定，临时增加一个自定义下载网址
 sed -i "s/push @mirrors, 'https:\/\/mirror2.openwrt.org\/sources';/&\\npush @mirrors, 'https:\/\/github.com\/xiaomeng9597\/files\/releases\/download\/iStoreosFile';/g" scripts/download.pl
-
-# 修改内核配置文件
-sed -i "/.*CONFIG_ROCKCHIP_RGA2.*/d" target/linux/rockchip/rk35xx/config-5.10
-
-# 集成CPU性能跑分脚本
-cp -a $GITHUB_WORKSPACE/configfiles/coremark/* package/base-files/files/bin/
-chmod 755 package/base-files/files/bin/coremark
-chmod 755 package/base-files/files/bin/coremark.sh
 
 # Lucky
 rm -rf package/lucky
@@ -21,9 +13,3 @@ git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 # OpenClash
 rm -rf package/OpenClash
 git clone --depth=1 https://github.com/vernesong/OpenClash package/OpenClash
-
-# emmc install
-if [ ! -d "package/base-files/files/usr/bin" ]; then
-    mkdir package/base-files/files/usr/bin
-fi
-cp -f ../SCRIPTS/emmc-install.sh package/base-files/files/usr/bin/emmc-install.sh
